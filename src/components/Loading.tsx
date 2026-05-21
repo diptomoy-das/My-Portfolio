93aa23f08c9c74b6595f063d3117a0eb41f03e0f
diptomoy-das <diptomoydad@gmail.com>
2026-05-21 23:16:09 +0530

    add logo image to loading screen

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -46,7 +46,7 @@ const Loading = ({ percent }: { percent: number }) => {
     <>
       <div className="loading-header">
         <a href="/#" className="loader-title" data-cursor="disable">
-          Logo
+          <img src="/images/logo.jpg" alt="Logo" className="loader-logo-img" />
         </a>
         <div className={`loaderGame ${clicked && "loader-out"}`}>
           <div className="loaderGame-container">
