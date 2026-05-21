12eaa2b47547eec590734a9223b9539a2b058979
diptomoy-das <diptomoydad@gmail.com>
2026-05-21 22:01:44 +0530

    added some fixes

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+declare module 'gsap-trial/SplitText' {
+  export class SplitText {
+    constructor(target: any, vars?: Record<string, any>);
+    chars: any[];
+    words: any[];
+    lines: any[];
+    revert(): void;
+    split(vars?: Record<string, any>): void;
+  }
+}
